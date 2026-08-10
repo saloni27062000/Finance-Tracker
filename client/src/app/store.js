1f74314447ee06ec600928d5a29b12a56e1e875e
Vaibhavee <vaibhavilambrud@gmail.com>
2026-08-10 15:57:58 +0530

    chore: save workspace changes

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,12 @@
 import authReducer from "../features/auth/authSlice";
 import bankReducer from "../features/bank/bankSlice";
-
+import transactionReducer from  "../features/transaction/transactionSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     bank: bankReducer,
+    transaction: transactionReducer,
   },
 });
